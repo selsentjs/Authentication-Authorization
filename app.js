abe4d5a84f392b14453e3d68ca05656b2c7e01ae
selvisentest2006 <selvisentest2006@gmail.com>
2024-11-25 18:39:12 +0530

    authentication and authorization

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config('');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+// router
+const authRouter = require('./Routes/authRoute')
+const userRouter = require('./Routes/userRoute')
+
+// database
+require('./Databases/conn');
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(cookieParser(process.env.JWT_SECRET))
+
+app.get('/', (req,res) => {
+    res.status(200).json("Welcome to my project")
+})
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
